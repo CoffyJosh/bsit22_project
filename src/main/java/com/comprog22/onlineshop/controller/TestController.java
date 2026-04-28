23d6df21df6de3850d20df7a81b81f1d428803c9
CoffyJosh <jrcofreros@gmail.com>
2026-04-28 15:43:05 +0800

    Testing Branch Made

--- src/main/java/com/comprog22/onlineshop/controller/TestController.java
+++ src/main/java/com/comprog22/onlineshop/controller/TestController.java
@@ -1,24 +1,34 @@
 package com.comprog22.onlineshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.comprog22.onlineshop.model.Student;
+import com.comprog22.onlineshop.services.EmailService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@RestController
+
+@Controller
 public class TestController {
-
-    @PostMapping("/addStudent")
-    public String addStudent(@RequestBody Student student) {
-        return String.format("INFORMATION TEST:\nName: %s\nAge: %d\nCourse: %s\n", student.getName(), student.getAge(), student.getCourse());
-    }
 
     @GetMapping("/helloWorld")
     public String getMethodName() {
         return "Hello World my Niggas!";
     }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+    
+  
 }
     
